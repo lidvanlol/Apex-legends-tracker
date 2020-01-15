c12f39e728dee6b97f562726d50f4e1d3aca9f11
Filip <lidvanlol@gmail.com>
2020-01-15 17:02:41 +0100

    first commit

---
+++ client/src/components/Search.vue
@@ -0,0 +1,55 @@
+<template>
+<section class="search">
+    <h1>Track Player Status</h1>
+    <form v-on:submit.prevent='onSubmit'>
+        <div class="form-group">
+            <label for="platform">Platform</label>
+            <select nmae="platform" id="platform" v-model="platform">
+                <option value="psn">Playstation</option>
+                                <option value="xbl">Xbox</option>
+                <option value="Origin">Origin</option>
+
+            </select>
+            </div> 
+            <div class="form-group">
+                <label for="gamertag">
+                    GamerTag
+                </label>
+                <input type='text' name='text' v-model='gamertag' id="gamertag" placeholder="">
+            </div>
+            <div class='form-group'>
+                <input type="submit" value="submit" class='btn'/>
+            </div>
+    </form>
+    </section>    
+</template>
+
+<script>
+export default {
+    name:'Search',
+    data(){
+        return{
+            platform:"psn",
+            gamertag:''
+        };
+    },beforeCreate(){
+        document.body.className = "body-bg-image";
+    },
+    methods:{
+        onSubmit(){
+            if(!this.gamertag){
+                this.$toasted.show("Please enter a gamertag",{
+                    duration:3000,
+                    icon:'exclamation-circle'
+                });
+
+            }else{
+                this.$router.push(`/profile/${this.platform}/${this.gamertag}`);
+            }
+        }
+    }
+}
+</script>
+
+<style>
+</style>
